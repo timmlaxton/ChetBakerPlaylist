c8540b818479c280b60dc28d90fb9bf3c809be55
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-09-06 14:09:41 +0100

    added side bar with song tracks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, {useState} from "react";
-import './styles/app.scss'
+import React, { useState } from "react";
+import "./styles/app.scss";
 import Player from "./components/Player";
 import Song from "./components/Songs";
-import data from "./utils"
-
-
+import Library from "./components/Library";
+import data from "./utils";
 
 function App() {
-
-const [songs, setSongs] = useState(data());
-const [currentSong, setCurrentSong] = useState(songs[0])
-const [isPlaying, setIsPlaying] = useState(false)
-
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player 
-      setIsPlaying={setIsPlaying}
-      isPlaying={isPlaying}
-      currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
+      <Library songs={songs} />
     </div>
   );
 }
